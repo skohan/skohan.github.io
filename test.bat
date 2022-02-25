0f7b86a2a2a5e3ae72dab9242c90866444efbe23
Sohan <sohan@email.com>
2022-02-25 21:54:03 +0530

    Added script for windows

---
+++ test.bat
@@ -0,0 +1,7 @@
+@echo off
+@REM #!/bin/sh
+@REM # command to start development server with live reload
+
+echo Starting server with live reload
+
+bundle exec jekyll serve --livereload
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# command to start development server with live reload
-
-echo Starting server with live reload
-
-bundle exec jekyll serve --livereload
